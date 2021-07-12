c62cbb859b383fc26f0357ddae8bf60d8a345b0b
HayashiNaok <b201409@hiroshima-u.ac.jp>
2021-07-12 22:06:13 +0900

    Release to heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,3 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery
-//= require moment
-//= require fullcalendar
-
